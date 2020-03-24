245af0fa0a68e396533e1859c9159af31ea61c23
Guilherme Flores Ferreira <guilherme.fflores@hotmail.com>
2020-03-24 15:16:46 +0000

    updated controllers and services require and export

--- src/services/index.js
+++ src/services/index.js
@@ -1,6 +1,6 @@
 const {readdirSync, lstatSync} = require('fs');
 readdirSync(__dirname + '/').forEach(function(file) {
     if (lstatSync(__dirname + '/' + file).isDirectory()) {
-        exports[`${file}Controller`] = require('./' + file);
+        exports[`${file}Service`] = require('./' + file);
     }
 });
