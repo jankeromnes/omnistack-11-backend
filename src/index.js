5bfe67ba822dc4c64406a6994bea69790cc34dd5
Guilherme Flores Ferreira <guilherme.fflores@hotmail.com>
2020-03-26 01:22:32 +0000

    added cors

--- src/index.js
+++ src/index.js
@@ -1,7 +1,9 @@
 require('dotenv-safe').config();
 const express = require('express');
+const cors = require('cors');
 
 const app = express();
+app.use(cors());
 app.use(express.json());
 
 const router = require('./routes/index');
