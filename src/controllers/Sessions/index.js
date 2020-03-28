d3c3d63b973e751850cab5c30057e78e47a7ea0e
Guilherme Flores Ferreira <guilherme.fflores@hotmail.com>
2020-03-28 17:22:13 +0000

    added encryption to login

--- src/controllers/Sessions/index.js
+++ src/controllers/Sessions/index.js
@@ -10,29 +10,33 @@ const OngsService = require('../../services/Ongs');
 
 module.exports = {
     async login(req, res){
-        const {id, password} = req.body;
+        const {id, cipherText} = req.body;
         if(!id){
             return res.status(BAD_REQUEST).json({
                 error: 'id não informado!',
             });
         }
-        if(!password){
+        if(!cipherText){
             return res.status(BAD_REQUEST).json({
                 error: 'Senha não informada!'
             });
         }
         try{
-            const ong = await OngsService.checkPassword({id, password});
+            const ong = await OngsService.checkPassword({
+                id, 
+                password: cipherText
+            });
             if(!ong){
                 return res.status(NOT_FOUND).json({
                     error: "Não foi encontrada uma ong com a combinação de id e senha informada!",
                 });
             }
+            const {name} = ong;
             const token = jwt.sign({id}, process.env.JWT_SECRET, {
                 expiresIn: 7776000, //3 months
             });
             res.header('X-Access-Token', token);
-            return res.json(ong);
+            return res.json({name});
         }catch(e){
             console.log(e);
             return res.status(INTERNAL_SERVER_ERROR).json({
--- src/services/Ongs/index.js
+++ src/services/Ongs/index.js
@@ -4,28 +4,23 @@ const Crypto = require('crypto');
 
 module.exports = {
     async create({name, email, whatsapp, city, uf, hex, iv}){
-        try {
-            const decipher = Crypto.createDecipheriv("aes-256-cbc", Buffer.from(email.repeat(Math.ceil(32 / email.length)).substring(0,32)), Buffer.from(iv, 'hex'));
-            const decoded = decipher.update(hex, "hex", 'utf8') + decipher.final('utf8');
-            const id = Crypto.randomBytes(16).toString('HEX');
-            const cipher = Crypto.createCipheriv("aes-256-cbc", id, process.env.DEFAULT_IV);
-            const password = cipher.update(decoded, 'utf8', 'hex') + cipher.final('hex'); 
-            await connection('ongs').insert(
-                {
-                    id,
-                    name,
-                    email,
-                    whatsapp,
-                    city,
-                    uf,
-                    password,
-                }
-            );
-            return id;
-        } catch (err) {
-            console.error(err);
-            return "";
-        }
+        const decipher = Crypto.createDecipheriv("aes-256-cbc", Buffer.from(email.repeat(Math.ceil(32 / email.length)).substring(0,32)), Buffer.from(iv, 'hex'));
+        const decoded = decipher.update(hex, "hex", 'utf8') + decipher.final('utf8');
+        const id = Crypto.randomBytes(16).toString('HEX');
+        const cipher = Crypto.createCipheriv("aes-256-cbc", Buffer.from(id, 'utf8'), Buffer.from(id, 'hex'));
+        const password = cipher.update(decoded, 'utf8', 'hex') + cipher.final('hex'); 
+        await connection('ongs').insert(
+            {
+                id,
+                name,
+                email,
+                whatsapp,
+                city,
+                uf,
+                password,
+            }
+        );
+        return id;
     },
     async listAll({offset, max}){
         const count = await connection('ongs').count();
@@ -50,7 +45,6 @@ module.exports = {
             .first()
     },
     async checkPassword({id, password}){
-        password = Crypto.sign('md5', password, id).toString();
         return await connection
             .select([...Ongs.publicFields, ...Ongs.privateFields])
             .from('ongs')
